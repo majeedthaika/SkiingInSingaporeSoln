4fbb8663d5389220dfd2ea79f97a9e43f3f2f3ff
Majeed Thaika <majeed.thaika@hotmail.com>
2016-09-05 23:49:17 +0700

    Add files via upload
    My 'hacky' solution to http://geeks.redmart.com/2015/01/07/skiing-in-singapore-a-coding-diversion/

---
+++ Main.java
@@ -0,0 +1,76 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+	static int[][] arr;
+	static int[][] dp;
+	
+	static int n;
+	static int m;
+
+	static int length;
+	static int drop;
+	
+	static void findPaths(int start, int len_so_far, int x, int y){
+		if (dp[x][y]==0){
+			if (length<len_so_far){
+				length = len_so_far;
+				drop = arr[x][y]-start;
+			} else if (length==len_so_far){
+				drop = Math.max(drop, arr[x][y]-start);
+			}
+		} else {
+			if (x!=0) if (dp[x][y]==(dp[x-1][y]+1)) findPaths(start, len_so_far+1, x-1, y);
+			if (y!=0) if (dp[x][y]==(dp[x][y-1]+1)) findPaths(start, len_so_far+1, x, y-1);
+			if (x!=(n-1)) if (dp[x][y]==(dp[x+1][y]+1)) findPaths(start, len_so_far+1, x+1, y);
+			if (y!=(m-1)) if (dp[x][y]==(dp[x][y+1]+1)) findPaths(start, len_so_far+1, x, y+1);
+		}
+	}
+	
+    public static void main(String[] args) throws java.lang.Exception {
+    	Scanner cin = new Scanner(System.in);
+
+    	n = cin.nextInt();
+    	m = cin.nextInt();
+    	
+    	length = -1;
+    	drop = -1;
+
+    	arr = new int[n][m];
+    	dp = new int[n][m];
+    	
+    	for (int i=0;i<n;i++){
+    		for (int j=0;j<m;j++){
+        		arr[i][j] = cin.nextInt();
+        	}
+    		Arrays.fill(dp[i], 0);
+    	}
+    	
+    	int maxPath = 0;
+    	for (int k=0;k<Math.max(n,m);k++){
+        	for (int i=0;i<n;i++){
+	    		for (int j=0;j<m;j++){
+	        		if (j!=0) if (arr[i][j]<arr[i][j-1]) dp[i][j] = Math.max(dp[i][j], dp[i][j-1]+1);
+	        		if (i!=0) if (arr[i][j]<arr[i-1][j]) dp[i][j] = Math.max(dp[i][j], dp[i-1][j]+1);
+	        		if (i!=(n-1)) if (arr[i][j]<arr[i+1][j]) dp[i][j] = Math.max(dp[i][j], dp[i+1][j]+1);
+	        		if (j!=(m-1)) if (arr[i][j]<arr[i][j+1]) dp[i][j] = Math.max(dp[i][j], dp[i][j+1]+1);
+	        		
+	        		maxPath = Math.max(maxPath, dp[i][j]);
+	        	}
+	    	}
+    	}
+
+    	ArrayList<String> starts = new ArrayList<String>();
+    	for (int i=0;i<n;i++){
+    		for (int j=0;j<m;j++){
+    			if (dp[i][j] == maxPath) starts.add(i+","+j);
+        	}
+    	}
+
+    	for(int i=0;i<starts.size();i++){
+    		String[] idx = starts.get(i).split(",");
+    		findPaths(arr[Integer.parseInt(idx[0])][Integer.parseInt(idx[1])], 1, Integer.parseInt(idx[0]),Integer.parseInt(idx[1]));
+    	}
+    	System.out.println(length+""+drop);
+    }
+}
