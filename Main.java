094378efee1e433d94a289db5346803e2c52ff8d
Majeed Thaika <majeed.thaika@hotmail.com>
2016-09-06 11:10:35 +0700

    Update Main.java
    Optimized revision

--- Main.java
+++ Main.java
@@ -1,5 +1,4 @@
 import java.util.*;
-import java.io.*;
 
 public class Main {
 	static int[][] arr;
@@ -18,7 +17,7 @@ public class Main {
 				drop = arr[x][y]-start;
 			} else if (length==len_so_far){
 				drop = Math.max(drop, arr[x][y]-start);
-			}
+			} else {}
 		} else {
 			if (x!=0) if (dp[x][y]==(dp[x-1][y]+1)) findPaths(start, len_so_far+1, x-1, y);
 			if (y!=0) if (dp[x][y]==(dp[x][y-1]+1)) findPaths(start, len_so_far+1, x, y-1);
@@ -39,25 +38,32 @@ public class Main {
     	arr = new int[n][m];
     	dp = new int[n][m];
     	
+    	int maxPeak = -1;
     	for (int i=0;i<n;i++){
     		for (int j=0;j<m;j++){
         		arr[i][j] = cin.nextInt();
+        		maxPeak = Math.max(maxPeak, arr[i][j]);
         	}
     		Arrays.fill(dp[i], 0);
     	}
     	
     	int maxPath = 0;
-    	for (int k=0;k<Math.max(n,m);k++){
+    	for (int k=0;k<Math.min(maxPeak,n*m);k++){
+    		boolean changed = false;
         	for (int i=0;i<n;i++){
 	    		for (int j=0;j<m;j++){
+	    			int before = dp[i][j];
 	        		if (j!=0) if (arr[i][j]<arr[i][j-1]) dp[i][j] = Math.max(dp[i][j], dp[i][j-1]+1);
 	        		if (i!=0) if (arr[i][j]<arr[i-1][j]) dp[i][j] = Math.max(dp[i][j], dp[i-1][j]+1);
 	        		if (i!=(n-1)) if (arr[i][j]<arr[i+1][j]) dp[i][j] = Math.max(dp[i][j], dp[i+1][j]+1);
 	        		if (j!=(m-1)) if (arr[i][j]<arr[i][j+1]) dp[i][j] = Math.max(dp[i][j], dp[i][j+1]+1);
 	        		
+	        		if (before != dp[i][j]) changed = true;
+	        		
 	        		maxPath = Math.max(maxPath, dp[i][j]);
 	        	}
 	    	}
+        	if (!changed) break;
     	}
 
     	ArrayList<String> starts = new ArrayList<String>();
